8bad8b6a0da8e26a56b928b597ff31e1e74f1c12
Md Mazidul Islam <mazid9326@gmail.com>
2025-02-27 11:16:57 +0600

    first commit

---
+++ tournament-C/src/main/java/com/mazid/tournament_C/TournamentCApplication.java
@@ -0,0 +1,13 @@
+package com.mazid.tournament_C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentCApplication.class, args);
+	}
+
+}
---
+++ tournament-C/src/test/java/com/mazid/tournament_C/TournamentCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazid.tournament_C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
